29e38f90e02e93d2e6a62a6342290ff0a09b0ea3
Hamza <h8982934@gmail.com>
2026-07-19 10:55:40 +0500

    fix: polyfill __dirname in webpack to prevent vercel ESM runtime error

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.node = {
+      ...config.node,
+      __dirname: true,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
